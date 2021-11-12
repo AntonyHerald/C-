2baeb9ee74b78aaf9509955233ce3613f84932cd
Antony Herald I <31497965+AntonyHerald@users.noreply.github.com>
2021-11-12 11:13:24 +0530

    Add files via upload

---
+++ 4.cppInheritance/inheri01.cpp
@@ -0,0 +1,43 @@
+// This program explains the base class reference 
+// when derived classes are assigned as pointer ref.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+class base{
+	public:
+	void display();
+};
+
+class der1 : public base{
+	public:
+	void display();
+};
+
+class der2: public base{
+	public:
+	void display();
+};
+
+void base::display() { cout <<"Base class Display"<<endl;}
+void der1::display() { cout <<"Der1 class Display"<<endl;}
+void der2::display() { cout <<"Der2 class Display"<<endl;}
+
+int main()
+{
+	base *B;
+	der1 D1;
+	der2 D2;
+
+       	B = &D1;
+	B->display();
+	B = &D2;
+	B->display();
+
+	return 0;
+}
+	
+	
